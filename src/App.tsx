82ad18662bd2718d653d15369fa026f92cce2d9f
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2020-12-13 19:11:51 +0530

    :zap: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+
+
+const App = () =>{
+  return(
+  <div> 
+    <Typography variant="h3">Electronjs + Reactjs with MaterialUI</Typography>
+    <Button variant="contained" color="primary">Now Start Coding</Button>
+  </div>
+
+  )
+}
+
+
+
+export default App
